45b99dfa6c030c2dd72222a7758e2e707630bb09
kelyan-9560 <55031705+kelyan-9560@users.noreply.github.com>
2021-03-30 20:35:15 +0200

    Add Models 3

---
+++ models/user.ts
@@ -0,0 +1,39 @@
+import {DataTypes, Model, ModelCtor, Optional, Sequelize} from "sequelize";
+
+export interface VisitProps {
+    id: number;
+    name: string;
+    password: string;
+    is_handicapped: boolean;
+
+}
+
+export  interface UserCreationProps extends Optional<VisitProps, "id"> {}
+
+export interface UserInstance extends Model<VisitProps, UserCreationProps>, VisitProps {
+
+}
+
+export default function (sequelize: Sequelize): ModelCtor<UserInstance> {
+    return sequelize.define<UserInstance>("User", {
+        id: {
+            type: DataTypes.BIGINT,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+        },
+        password: {
+            type: DataTypes.STRING,
+        },
+        is_handicapped: {
+            type: DataTypes.BOOLEAN,
+        }
+    }, {
+        freezeTableName: true,
+        underscored: true,
+        paranoid: true,
+        timestamps: true
+    });
+}
---
+++ models/visit.ts
@@ -0,0 +1,31 @@
+import {DataTypes, Model, ModelCtor, Optional, Sequelize} from "sequelize";
+
+export interface VisitProps {
+    id: number;
+    name: string;
+
+}
+
+export  interface VisitCreationProps extends Optional<VisitProps, "id"> {}
+
+export interface VisitInstance extends Model<VisitProps, VisitCreationProps>, VisitProps {
+
+}
+
+export default function (sequelize: Sequelize): ModelCtor<VisitInstance> {
+    return sequelize.define<VisitInstance>("Visit", {
+        id: {
+            type: DataTypes.BIGINT,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+        }
+    }, {
+        freezeTableName: true,
+        underscored: true,
+        paranoid: true,
+        timestamps: true
+    });
+}
