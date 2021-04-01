0268d7b902659f1e1ad586af44a60edae5da8829
Xof <jehanno.lucas@gmail.com>
2021-04-01 18:56:56 +0200

    get set familly  dans model animal

--- models/animal.ts
+++ models/animal.ts
@@ -12,6 +12,7 @@ import {
     HasOneSetAssociationMixin
 } from "sequelize";
 import {Animal_notebookInstance} from "./animal_notebook";
+import {FamilyInstance} from "./family";
 
 export interface AnimalProps{
     id:number;
@@ -22,6 +23,8 @@ export interface AnimalProps{
 export interface AnimalCreationProps extends Optional<AnimalProps, "id">{}
 
 export interface AnimalInstance extends Model<AnimalProps,AnimalCreationProps>,AnimalProps{
+    getFamily: HasOneGetAssociationMixin<FamilyInstance>;
+    setFamily: HasOneSetAssociationMixin<FamilyInstance, "id">;
 
     getAnimalNoteBook: HasOneGetAssociationMixin<Animal_notebookInstance>;
     setAnimalNoteBook: HasOneSetAssociationMixin<Animal_notebookInstance, "id">;
