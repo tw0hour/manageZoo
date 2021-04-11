9029bc0f7bbe9dd52229605933a348df9636873c
Xof <jehanno.lucas@gmail.com>
2021-04-12 00:20:56 +0200

    space routes (non testé)

--- routes/index.ts
+++ routes/index.ts
@@ -4,11 +4,13 @@ import {userRoutes} from "./userRoutes";
 import {typeRoutes} from "./typeRoutes";
 import {visitRoutes} from "./visitRoutes";
 import {animal_notebookRoutes} from "./animal_notebookRoutes";
+import {spaceRoutes} from "./spaceRoutes";
 
 export function buildRoutes(app: Express) {
     app.use("/visit",visitRoutes)
     app.use("/user",userRoutes);
     app.use("/animal", animalRoutes);
     app.use("/type",typeRoutes);
-    app.use("/animal_notebook",animal_notebookRoutes)
+    app.use("/animal_notebook",animal_notebookRoutes);
+    app.use("/space",spaceRoutes);
 }
