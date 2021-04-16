55c7d1d83c179b9821b45c1d78d7db0ef08a13e6
Xof <jehanno.lucas@gmail.com>
2021-04-16 19:20:44 +0200

    Zoo controller

---
+++ controllers/zooController.ts
@@ -0,0 +1,94 @@
+import {ModelCtor} from "sequelize";
+import {SequelizeManager} from "../models";
+import {ZooCreationProps, ZooInstance} from "../models/zoo";
+
+
+export interface ZooUpdateOption {
+    id:string;
+    name?:string;
+    isOpen?:string;
+}
+
+export class ZooController {
+
+    Zoo: ModelCtor<ZooInstance>;
+
+    private static instance: ZooController;
+
+    public static async getInstance(): Promise<ZooController> {
+        if(ZooController.instance == undefined) {
+            const {Space} = await SequelizeManager.getInstance();
+            ZooController.instance = new ZooController(Space);
+        }
+        return  ZooController.instance;
+    }
+
+    constructor(Zoo: ModelCtor<ZooInstance>) {
+        this.Zoo = Zoo;
+    }
+
+    public async getAll(limit: number, offset: number): Promise<ZooInstance[] | null>{
+        return await this.Zoo.findAll({
+            limit,
+            offset
+        });
+    }
+
+    public async add(props: ZooCreationProps): Promise<ZooInstance | null> {
+        return await this.Zoo.create({
+            ...props
+        });
+    }
+
+    public async getById(id: string): Promise<ZooInstance | null> {
+        return await this.Zoo.findOne({
+            where :{
+                id: id
+            }
+        });
+    }
+    public async update(options: ZooUpdateOption): Promise<ZooInstance | null> {
+
+        const zooUpdate = await this.getById(options.id);
+
+        if(zooUpdate === null)
+        {
+            return null;
+        }
+        else
+        {
+            return await zooUpdate.update({
+                ...options
+            }, {
+                where: {
+                    id: options.id
+                }
+            });
+        }
+    }
+
+    public async removeById (id: string):Promise<Boolean> {
+        const zooDelete = await this.getById(id);
+        if(zooDelete === null)
+        {
+            return false;
+        }
+        else
+        {
+            try
+            {
+                await this.Zoo.destroy({
+                    where:{
+                        id: zooDelete.id
+                    }
+                });
+                return true;
+            }
+            catch (err)
+            {
+                console.error(err);
+                return false;
+            }
+        }
+    }
+}
